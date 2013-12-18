841a6b93653c469e8beb88488d68ace9efd51ef7
Amogh Chakravarthy <achakravarthy@zynga.com>
2013-12-18 16:39:58 +0530

    Adding a new file

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+Adding a new file to the project
+
+author: achakravarthy
+*/
+
+public class HelloWorld {
+
+	public static void main (String [] args) {
+
+		System.out.println ("Hello World");
+
+	} // end method main
+
+} // end class HelloWorld
--- readme.txt
+++ readme.txt
@@ -1,5 +1,6 @@
-readme new git project
+New git project
 changes
 more changes
 bugfix in master
 working on new branch
+Adding HelloWorld.java
